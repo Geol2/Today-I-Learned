65507833f13de012d6643ae6e3e2a060815e30ea
geol2 <big9401@gmail.com>
2022-12-10 21:58:43 +0900

    [JAVA] 객체 추가

--- Java/Basic/Profile.java
+++ Java/Basic/Profile.java
@@ -1,6 +1,29 @@
-public class Profile {
+class Profile {
+    String name;
+    int age;
+    
+    public void setName(String str) {
+        this.name = str;
+    }
+
+    public void setAge(int val) {
+        this.age = val;
+    }
+
+    public void printName() {
+        System.out.println("My name is " + this.name);
+    }
+
+    public void printAge() {
+        System.out.println("My age is " + this.age);
+    }
+
     public static void main(String[] args) {
-        System.out.println("My name is GEOL");
-        System.out.println("My age is 28"); 
+        Profile profile = new Profile();
+        profile.setName("Min");
+        profile.setAge(20);
+
+        profile.printName();
+        profile.printAge();
     }
 }
