c7e921fff67741d784cd3e811bce1e9877ed4c6f
geol2 <big9401@gmail.com>
2022-12-18 22:26:04 +0900

    [JAVA] AbstractCustomList.java 수정

--- Java/Basic/customList/AbstractCustomList.java
+++ Java/Basic/customList/AbstractCustomList.java
@@ -1,16 +1,5 @@
 package Java.Basic.customList;
 
 public abstract class AbstractCustomList implements CustomList{
-    public void add() {
-        // ? 
-    }
-    public void update(int index, Object value) {
-        // ? 
-    }
-    public void remove(int index) {
-        // ? 
-    }
-    public void clear() {
-        // ? 
-    }
+    public abstract void clear();
 }
